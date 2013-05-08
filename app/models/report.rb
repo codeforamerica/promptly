35ae48adb531422360b7f262c90c80e32b73bc8f
Andy Hull <andyhull3@gmail.com>
2013-05-08 10:44:19 -0700

    adding basic new and edit nested forms

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,5 @@
 class Report < ActiveRecord::Base
-  attr_accessible :humanname, :type
+  attr_accessible :humanname, :report_type
+  has_and_belongs_to_many :recipients
+  has_many :programs, :through => :recipients
 end
