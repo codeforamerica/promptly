f3659ed26f4a59eb4366ed1ede42d214e9ca12b4
Andy Hull <andyhull3@gmail.com>
2013-05-09 12:13:21 -0700

    new testing branchgit add .

--- spec/models/recipient_spec.rb
+++ spec/models/recipient_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
+
 describe Recipient do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before :each do
+	    @recipient = Recipient.new(:phone => +19196361635, :case => 1234)
+	end
+  it "should have a phone number" do
+    @recipient.phone.should_not be_nil
+  end
 end
