b4fa2df042e8454c03acc8fb134a87c9a9ee6a31
Andy Hull <andyhull3@gmail.com>
2013-05-09 08:46:27 -0700

    added basic verification of phone number

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -3,7 +3,7 @@ class TextController < ApplicationController
   end
  
   def send_text_message
-    number_to_send_to = +19196361635
+    # number_to_send_to = +19196361635
  
     twilio_sid = ENV['TWILIO_SID']
     twilio_token = ENV['TWILIO_TOKEN']
@@ -22,20 +22,39 @@ class TextController < ApplicationController
     message_body = params["Body"]
     from_number = params["From"]
     if from_number
-      twilio_sid = ENV['TWILIO_SID']
-      twilio_token = ENV['TWILIO_TOKEN']
-      twilio_phone_number = ENV['TWILIO_NUMBER']
-
-      @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
-      # binding.pry
-      @twilio_client.account.sms.messages.create(
-        :from => "+1#{twilio_phone_number}",
-        :to => from_number,
-        :body => "We got your message! '#{message_body}' Thanks!"
-      )
+      verify_recipient(from_number)
     else
       redirect_to(recipients_path)
     end
+    # Log this conversation
     # SMSLogger.log_text_message from_number, message_body
+  end
+
+  private
+
+  def verify_recipient(phone_number)
+    @recipients = Recipients.find(find(:all, :conditions => ["where phone = ?", twilio_phone_number]))
+    if @recipients
+      send_reply(phone_number, params["Body"])
+    else
+      send_reply(phone_number, "Sorry we couldn't verify your number.")
+    end
+
+  end
+
+  private 
+
+  def send_reply(phone_number, message)
+    twilio_sid = ENV['TWILIO_SID']
+    twilio_token = ENV['TWILIO_TOKEN']
+    twilio_phone_number = ENV['TWILIO_NUMBER']
+
+    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+    # binding.pry
+    @twilio_client.account.sms.messages.create(
+      :from => "+1#{twilio_phone_number}",
+      :to => phone_number,
+      :body => "We got your message! '#{message}'"
+    )
   end
 end
