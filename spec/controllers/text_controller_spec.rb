02cabb2869d214639eef7fe6281926367d4fbde9
Andy Hull <andyhull3@gmail.com>
2013-05-09 14:23:51 -0700

    updating readme

--- spec/controllers/text_controller_spec.rb
+++ spec/controllers/text_controller_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe TextController do
 	it "checks if there is a valid incoming phone number" do
-		get :receive_message
+		get :receive_text_message, :phone => 9196361635
+		# flash[:alert].response
+		expect(response.body).to include(9196361635.to_s)
 	end
 end
