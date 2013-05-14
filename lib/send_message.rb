8b644b7d508ba3c93fd8e0919e8066ecc612b9b1
Andy Hull <andyhull3@gmail.com>
2013-05-14 15:17:13 -0700

    testing delayed job

---
+++ lib/send_message.rb
@@ -0,0 +1,16 @@
+class SendMessage < Struct.new(:from_number, :to_number, :reminder)
+  def perform
+  	twilio_sid = ENV['TWILIO_SID']
+    twilio_token = ENV['TWILIO_TOKEN']
+    twilio_phone_number = ENV['TWILIO_NUMBER']
+
+    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+    # binding.pry
+    @twilio_client.account.sms.messages.create(
+      :from => :from_number,
+      :to => :to_number,
+      :body => :reminder
+    )
+
+  end
+end
