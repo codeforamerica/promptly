84a530c3b05b04f8544f4bb3b4bdf1f0a67be789
Andy Hull <andyhull3@gmail.com>
2013-05-16 19:12:47 -0700

    fixing syntax error

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -25,7 +25,7 @@ class TextController < ApplicationController
   def verify_recipient(phone_number)
     @recipients = Recipient.find(:all, :conditions => ["phone = ?", phone_number.gsub('+1','').to_i])
     if @recipients
-      @recipients do |recipient|
+      @recipients.each do |recipient|
        Notifier.perform(@recipient, "Thanks we'll remind you of your report on: #{@recipient.reminder_date.to_s(:date_format)}.")
       end
     else
