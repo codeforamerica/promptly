51c0a6f87a1f8ed6421451b713067de8f650d537
rcackerman <rebecca@chapin>
2013-05-18 19:18:22 -0700

    Adding partials to the admin dashboard.
    Recent conversations through index method now works.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,28 @@
 class HomeController < ApplicationController
   def index
+    @recents = Conversation.all
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @recents }
+    end
   end
 
   def add_user
   end
 
   def list_upcoming
+  	# list queued
   end
 
+  # GET /conversations
   def list_recent
+  	@recents = Conversation.all
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @recents }
+    end
   end
 
 end
