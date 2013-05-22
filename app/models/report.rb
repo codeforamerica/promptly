a0635bddb1bbd68a914ce0840217611013850c4b
Andy Hull <andyhull3@gmail.com>
2013-05-22 16:22:30 -0700

    added reports to recipients/new. changed homepage text closing issue #21

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,5 @@
 class Report < ActiveRecord::Base
-  attr_accessible :humanname, :report_type
+  attr_accessible :humanname, :report_type, :recipient_ids, :recipient_attributes
   has_and_belongs_to_many :recipients
   has_many :programs, :through => :recipients
 end
