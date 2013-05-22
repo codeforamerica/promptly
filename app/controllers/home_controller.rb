01d35a3d668d3e4610e81f14bf24bd53a69d7239
Andy Hull <andyhull3@gmail.com>
2013-05-21 21:12:58 -0700

    fixing habtm associations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     @add_recipient = Recipient.all
     @recents = Conversation.all
+    @upcoming = Delayed::Job.find :all , :conditions=>"locked_at IS NOT NULL"
   end
 
 end
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,9 @@
 class Conversation < ActiveRecord::Base
   attr_accessible :date, :message, :to_number, :from_number
-  has_many :recipients
+  has_and_belongs_to_many :recipients
   has_many :reports
   has_many :programs
+
+  attr_accessible :recipient_ids
+  attr_accessible :conversation_ids
 end
