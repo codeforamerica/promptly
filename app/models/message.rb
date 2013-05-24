b7b8f3d996882c95a1efe328c849f0be9ff11513
Andy Hull <andyhull3@gmail.com>
2013-05-24 08:05:32 -0700

    fixing messages table

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
-  attr_accessible :message, :report_id, :send_date, :type
-  belongs_to :reports
+  attr_accessible :messagetext, :report_id, :send_date, :type
+  belongs_to :report
   has_many :recipients
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,8 @@
 class Report < ActiveRecord::Base
-  attr_accessible :humanname, :report_type, :recipient_ids, :recipient_attributes
+  attr_accessible :humanname, :report_type, :recipient_ids, :recipients_attributes, :program_ids
   has_and_belongs_to_many :recipients
   has_many :programs, :through => :recipients
+  has_many :messages
+  accepts_nested_attributes_for :messages
+  attr_accessible :message_attributes
 end
