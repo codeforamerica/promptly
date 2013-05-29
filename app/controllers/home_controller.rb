08d3f7d0486ea9327b38fd09f46a09bf554798d4
Andy Hull <andyhull3@gmail.com>
2013-05-28 18:00:00 -0700

    working on notifications model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @add_recipient = Recipient.all
     @recents = Conversation.all
-    @upcoming = Delayed::Job.find :all , :conditions=>"locked_at IS NOT NULL"
+    @upcoming = Notifications.all
   end
 
 end
