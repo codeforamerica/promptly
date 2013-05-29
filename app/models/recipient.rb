162701a926ba46263abd2fe78495aa148cd2863a
Andy Hull <andyhull3@gmail.com>
2013-05-28 23:24:36 -0700

    adding basic spreadsheet upload

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -1,5 +1,5 @@
 class Recipient < ActiveRecord::Base
-  attr_accessible :active, :case, :phone, :reminder_date
+  attr_accessible :active, :case, :phone, :reminder_date, :report_attributes
   has_and_belongs_to_many :reports
   has_and_belongs_to_many :conversations
   has_and_belongs_to_many :programs
@@ -9,4 +9,27 @@ class Recipient < ActiveRecord::Base
   attr_accessible :program_ids
 
   # validates :phone, :presence => true
+
+  def self.import(file)
+	  spreadsheet = open_spreadsheet(file)
+	  header = spreadsheet.row(1)
+	  (2..spreadsheet.last_row).each do |i|
+	    row = Hash[[header, spreadsheet.row(i)].transpose]
+	    recipient = where(phone: row["phone"])
+        .first_or_create!(row.to_hash.slice(*accessible_attributes))
+        # binding.pry
+
+      recipient.reports.where(report_type: row['report_name'])
+        .first_or_create!(row.to_hash.slice(*recipient.reports.accessible_attributes))
+	  end
+	end
+
+	def self.open_spreadsheet(file)
+	  case File.extname(file.original_filename)
+		  when ".csv" then Roo::Csv.new(file.path, nil, :ignore)
+		  when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
+		  when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
+		  else raise "Unknown file type: #{file.original_filename}"
+	  end
+	end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,5 @@
 class Report < ActiveRecord::Base
-  attr_accessible :humanname, :report_type, :recipient_ids, :recipients_attributes, :program_ids
+  attr_accessible :humanname, :report_type, :recipient_ids, :recipient_attributes, :program_ids
   has_and_belongs_to_many :recipients
   has_many :programs, :through => :recipients
   has_many :messages
