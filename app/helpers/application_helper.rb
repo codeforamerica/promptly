9e24ddc876d815f2295b9a54caeecaba7e0cb69f
Andy Hull <andyhull3@gmail.com>
2013-06-03 15:39:45 -0700

    created sendNotification method in application controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	def sendNotification(notification, report, recipient)
+	  # Notifier.perform(recipient, "Your #{report.humanname} report is due #{@notification.send_date.to_s(:date_format)}. We will remind you one week before. Text STOP to stop these text messages.")
+	  if notification.send_date < DateTime.now
+	    Notifier.perform(recipient, "Your #{report.humanname} report is due on Monday, May 27th. Need help? Call (415) 558-1001.")
+	  else
+	    # use Notifier.new here so delayed job can hook into the perform method
+	    Delayed::Job.enqueue(Notifier.new(recipient, "Your #{report.humanname} report is due #{recipient.notifications.send_date.to_s(:date_format)}. Need help? Call (415) 558-1001."), notification.send_date)
+	  end
+	end
 end
