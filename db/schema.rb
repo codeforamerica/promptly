5b74c6c2a4134e780634427969c3d6a6c4d76579
rcackerman <rebecca@chapin>
2013-06-06 16:21:06 -0700

    Adding changes made by db:migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -115,5 +115,4 @@ ActiveRecord::Schema.define(:version => 20130606172825) do
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
-
 end
