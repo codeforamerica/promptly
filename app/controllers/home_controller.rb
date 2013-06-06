4690404ac6f7990f1a4166095f42e3772bcf0176
Andy Hull <andyhull3@gmail.com>
2013-06-06 16:47:32 -0700

    limiting the number of conversations on the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @recipient = Recipient.all
-    @recents = Conversation.all
+    @recents = Conversation.find(:all, :order => "id desc", :limit => 5).reverse
     @upcoming = Notification.where("send_date >= ?", DateTime.now)
     @report = Report.all
   end
