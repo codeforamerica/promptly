dc50973a16c5b472d7c5a6a1c32e6cf60450fd58
rcackerman <rebecca@cfa>
2013-06-10 16:37:01 -0700

    Adding nested form attributes to program model.

--- app/models/program.rb
+++ app/models/program.rb
@@ -2,6 +2,6 @@ class Program < ActiveRecord::Base
   attr_accessible :description, :name, :report_ids, :report_id
   has_many :reports
   has_and_belongs_to_many :recipients
-
+  accepts_nested_attributes_for :reports, :allow_destroy => true
   attr_accessible :recipient_ids
 end
