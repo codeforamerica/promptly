9ff535883ab04a09889ca93f26728cadd009d1d8
Andy Hull <andyhull3@gmail.com>
2013-06-12 11:51:33 -0700

    new delayed job logic

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -55,10 +55,12 @@ class RecipientsController < ApplicationController
         format.json { render json: @recipient, status: :created, location: @recipient }
         @recipient.reports.try(:each) do |report|
           if @notification.send_date < DateTime.now
-            Delayed::Job.enqueue(Notifier.new(@recipient, Message.find_by_report_id(report.id).messagetext), DateTime.now)
+            Notifier.delay(priority: 1, run_at: DateTime.now).new(@recipient, Message.find_by_report_id(report.id).messagetext)
+            # Delayed::Job.enqueue(Notifier.new(@recipient, Message.find_by_report_id(report.id).messagetext), DateTime.now)
           else
             # use Notifier.new here so delayed job can hook into the perform method
-             Delayed::Job.enqueue(Notifier.new(@recipient, Message.find_by_report_id(report.id).messagetext), @notification.send_date)
+            Notifier.delay(priority: 1, run_at: @notification.send_date).new(@recipient, Message.find_by_report_id(report.id).messagetext)
+             # Delayed::Job.enqueue(Notifier.new(@recipient, Message.find_by_report_id(report.id).messagetext), @notification.send_date)
             Notifier.notification_add
           end
         end
--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -63,14 +63,4 @@ class Recipient < ActiveRecord::Base
 	  end
 	end
 
-def self.sendNotification(sendDate, report, recipient)
-    # Notifier.perform(recipient, "Your #{report.humanname} report is due #{@notification.send_date.to_s(:date_format)}. We will remind you one week before. Text STOP to stop these text messages.")
-    if sendDate < DateTime.now
-      Notifier.perform(recipient, "Your #{report.humanname} report is due on Monday, May 27th. Need help? Call (415) 558-1001.")
-    else
-      # use Notifier.new here so delayed job can hook into the perform method
-      Delayed::Job.enqueue(Notifier.new(recipient, "Your #{report.humanname} report is due #{sendDate.to_s(:date_format)}. Need help? Call (415) 558-1001."), sendDate)
-    end
-  end
-
 end
