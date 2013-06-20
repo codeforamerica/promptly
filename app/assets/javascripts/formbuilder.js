6b717f1cbead2a03f96b10537ac5ef512e4dd756
rcackerman <rebecca@cfa>
2013-06-20 18:22:19 -0400

    Redoing reminder form to use has_one associations.

--- app/assets/javascripts/formbuilder.js
+++ app/assets/javascripts/formbuilder.js
@@ -1,16 +1,16 @@
-// $(function() {
-//   // Handler adding form fields
-// 	$('form .add_fields').on('click', function(event) {
-// 		time = new Date().getTime();
-// 		regexp = new RegExp($(this).data('id'), 'g');
-// 		$(this).before($(this).data('fields').replace(regexp, time));
-// 		event.preventDefault();
-// 		$(this).hide()
-// 	});
+$(function() {
+  // Handler adding form fields
+	$('form .add_fields').on('click', function(event) {
+		time = new Date().getTime();
+		regexp = new RegExp($(this).data('id'), 'g');
+		$(this).before($(this).data('fields').replace(regexp, time));
+		event.preventDefault();
+		$(this).hide()
+	});
 
-// 	$('form .remove_fields').on('click', function(event) {
-//     $(this).prev('input[type=hidden]').val('1');
-//     $(this).closest('fieldset').hide();
-//     event.preventDefault();
-// 	});
-// });
+	$('form .remove_fields').on('click', function(event) {
+    $(this).prev('input[type=hidden]').val('1');
+    $(this).closest('fieldset').hide();
+    event.preventDefault();
+	});
+});
