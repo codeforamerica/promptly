4ee3d30e42a0fd433769feb188903d6eb25ebc1f
Andy Hull <andyhull3@gmail.com>
2013-06-26 17:16:42 -0700

    removing dumb migration

--- db/migrate/20130626011341_add_message_text_to_reminder.rb
+++ db/migrate/20130626011341_add_message_text_to_reminder.rb
@@ -1,6 +1,5 @@
 class AddMessageTextToReminder < ActiveRecord::Migration
   def change
-  	remove_column :reminders, :messsage_text, :string
   	add_column :reminders, :message_text, :string
   end
 end
