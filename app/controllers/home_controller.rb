2a8adcbee0ce296d9e1691c5436a681fbf12e639
rcackerman <rebecca@cfa>
2013-06-27 11:57:07 -0700

    Filtering recent messages to be only ones sent out.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @recipient = Recipient.all
-    @recents = Conversation.find(:all, :order => "date desc", :limit => 10)
+    @recents = Conversation.where(:from_number => '14154198992').find(:all, :order => "date desc", :limit => 10)
     @upcoming = Notification.where("send_date >= ?", DateTime.now)
     @report = Report.all
   end
