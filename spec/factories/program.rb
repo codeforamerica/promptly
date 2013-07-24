87e69ee2e25c9fd3a49f342e113fc14d128298a1
rcackerman <rebecca@cfa>
2013-07-24 09:29:58 -0700

    Working on reminder spec. Broken for now.

--- spec/factories/program.rb
+++ spec/factories/program.rb
@@ -3,5 +3,7 @@ FactoryGirl.define do
   factory :program do
     name "test"
 		description "a test"
+
+		report
   end
 end
--- spec/factories/report.rb
+++ spec/factories/report.rb
@@ -2,6 +2,5 @@ FactoryGirl.define do
   factory :report do
   	report_type "QR-7"
 		humanname "Quarterly Report"
-		program { FactoryGirl.create(:program)}
   end
 end
