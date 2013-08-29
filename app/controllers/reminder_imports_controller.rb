aa78d3909e3e1aefcb30eb1c6f6644320c0e6b65
Andy Hull <andyhull3@gmail.com>
2013-08-29 14:52:12 -0700

    added state flag to reminders

--- app/controllers/reminder_imports_controller.rb
+++ app/controllers/reminder_imports_controller.rb
@@ -1,5 +1,7 @@
 class ReminderImportsController < ApplicationController
   include ActionView::Helpers::TextHelper
+  
+  attr_accessor :valid_records, :valid
 
   def new
     @reminder_import = ReminderImport.new
@@ -8,7 +10,7 @@ class ReminderImportsController < ApplicationController
   end
 
   def review
-    @reminder_import = ReminderImport.new(params[:reminder_import], params[:message_id])
+    @reminder_import = ReminderImport.new(params[:reminder_import], params[:message_id], session)
     @reminder_import.review
   end
 
@@ -17,21 +19,15 @@ class ReminderImportsController < ApplicationController
     fail = 0
     @reminder_import = params[:reminder_import]
     @reminder_import.map do |r|
-      if save_new_reminders(r[1]).is_a? Reminder
-        success = success + 1
+      if ReminderImport.save_new_reminders(r[1]).is_a? Reminder
+        success += 1
       else
-        fail = fail + 1
+        fail += 1
       end
     end
     @success = success
     @fail = fail
     redirect_to reminders_url, notice: "Imported #{pluralize(success, 'reminder')} successfully. #{pluralize(fail, 'failure')}"
-  end
-
-  def save_new_reminders(reminders)
-    recipient = Recipient.where(phone: reminders['phone']).first_or_create
-    text_message = Message.find(reminders['message'])
-    Reminder.create_new_recipients_reminders(recipient, reminders['send_date'], send_time = '12:00pm', text_message)    
   end
 
 end
