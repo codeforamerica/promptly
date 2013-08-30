22b8dbdc565b52c934c9b69d344a92bf3a4875cd
Andy Hull <andyhull3@gmail.com>
2013-08-30 10:35:44 -0700

    removing pry call

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
   def index
     @recipient = Recipient.all
     @conversations = Conversation.find(:all, :order => "date desc", :limit => 10)
-    @upcoming = Reminder.grouped_reminders.collect
-    binding.pry
+    @upcoming = Reminder.grouped_reminders.collect{ |reminder| reminder['send_date'] >= DateTime.now}
   end
 end
