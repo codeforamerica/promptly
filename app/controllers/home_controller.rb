eb6d20af1b44c144a1b0fb72051b54da2e8519b0
Andy Hull <andyhull3@gmail.com>
2013-08-30 08:47:35 -0700

    added send time to batch import

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
     @recipient = Recipient.all
     @conversations = Conversation.find(:all, :order => "date desc", :limit => 10)
     @upcoming = Reminder.grouped_reminders.collect
+    binding.pry
   end
 end
