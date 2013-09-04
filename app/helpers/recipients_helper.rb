eb2b718c719f8edf9feaab56a533d4150d626592
rcackerman <rebecca@cfa>
2013-09-04 14:53:58 -0700

    Starting to style conversations.

--- app/helpers/recipients_helper.rb
+++ app/helpers/recipients_helper.rb
@@ -2,4 +2,14 @@ module RecipientsHelper
 	def notifications
 		@recipient.notifications
 	end
+
+	def which_class(phone_number)
+		self_phone = ENV["TWILIO_NUMBER"]
+		if phone_number[-10..-1] != self_phone
+			css_class = "conversation-recipient"
+		else
+			css_class = "conversation-self"
+		end
+	end 
+
 end
