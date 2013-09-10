3c700a23a97ef94c9a83daa73f1e90742e4260bf
rcackerman <rebecca@cfa>
2013-09-09 17:16:02 -0700

    Fixing seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Reminder.create(name: 'test', message_text: 'Hi, this is a test', description: 'First test reminder')
 
