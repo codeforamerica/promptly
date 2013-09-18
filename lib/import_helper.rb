b19e299048156bd0bd9ed82288a653ba344092a2
Jake Solomon <jacob@codeforamerica.org>
2013-09-18 15:29:12 -0700

    1. schedule.rb defines cron job schedule (run 'whenever --update-crontab' to start, 'whenever -c' to stop)
    2. The cronjob(s) run the import rake task: rake 'import_group[DB_URL,TABLE,GROUP_NAME_ID]'
    
    3. The rake task runs the import method (ImportHelper.update_or_create_group_from_db_table with params)
    
    4. The import method does the heavy lifting of connecting to the external DB, grabbing phone numbers, matching/creating recipients, and updating/creating the group.

--- lib/import_helper.rb
+++ lib/import_helper.rb
@@ -1,30 +1,50 @@
 class ImportHelper < ActiveRecord::Base
   
-  def self.load_deliveries_from_db_table(db_url, table)
+  def self.update_or_create_group_from_db_table(db_url, table, group_name_id)
+    puts "#{Time.now.to_formatted_s} - STARTING GROUP IMPORT
+    Group: #{group_name_id}
+    From DB: #{db_url}
+    From table: #{table}"
+    
     #connect to DB
-    puts "Connecting to DB..."
+    puts "Connecting to external source."
     establish_connection(ENV[db_url])
     con = connection()
 
     #get SQL result
+    puts "Querying DB."
     sql = "SELECT * FROM #{table}"
     result = con.select_all(sql)
 
     #reconnect to rails DB
+    puts "Reconnecting to rails DB."
     remove_connection
     establish_connection(ENV['DATABASE_URL'])
 
-    #export csv
-    filepath = "app/imports/#{table}_lastimport.csv"
-    CSV.open("#{filepath}", "wb") do |csv|
-      csv << result.first.keys # headers
-      result.each do |hash|
-        csv << hash.values
+    #save as array of phone number
+    puts "Converting phone numbers to array."
+    phone_numbers = []
+    result.each do |p_hash|
+      val = p_hash.values.first
+      if val
+        phone_numbers << p_hash.values.first
       end
     end
 
-    #import csv
-    delivery_import = DeliveryImport.new
-    delivery_import.load_deliveries_from_csv(filepath)
+    # Find or create group
+    g = Group.where(group_name_id: group_name_id).first
+    g ||= Group.new(
+      :group_name_id => group_name_id,
+      :name => group_name_id,
+      :description => "Imported from #{table}",
+      :editable => 0
+      )
+    g.save
+    puts "Modifying group: #{g.group_name_id}"
+
+    # Save phone numbers to group
+    puts "Adding phone numbers: #{phone_numbers}"
+    g.add_phone_numbers(phone_numbers)
+    puts "#{Time.now.to_formatted_s} - FINISHED GROUP IMPORT"
   end
 end
