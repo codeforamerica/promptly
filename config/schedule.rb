d6cb9a38ab058f5917f32d6db5c09eb3b1e77db8
Jake Solomon <jacob@codeforamerica.org>
2013-09-18 15:53:28 -0700

    remove every 1.day import from schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,7 +4,3 @@ every 1.minute do
   rake "import_group[CIS_DATABASE_URL,dc_test_calfresh_english,dc_test_calfresh_english]", :environment => 'development'
   rake "import_group[CIS_DATABASE_URL,dc_test_calfresh_span,dc_test_calfresh_spanish]", :environment => 'development'
 end
-
-every 1.day do
-  rake "import_group[CIS_DATABASE_URL,dc_test_calfresh_english,dc_test_calfresh_english]", :environment => 'staging'
-end
