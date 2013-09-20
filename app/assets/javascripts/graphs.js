f813ef574ce82cc03825d04da44954d98f18e9d5
Andy Hull <andyhull3@gmail.com>
2013-09-20 10:26:41 -0700

    fixed dashboard charts

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -1,8 +1,3 @@
-$(function() {
-  upcomingMessages();
-  sentMessages()
-});
-
 function upcomingMessages(){
   new Morris.Line({
     // ID of the element in which to draw the chart.
@@ -24,6 +19,22 @@ function sentMessages(){
     // ID of the element in which to draw the chart.
     element: 'sent_chart',
     data: $('#sent_chart').data('sent'),
+    // The name of the data record attribute that contains x-values.
+    xkey: 'date',
+    // A list of names of data record attributes that contain y-values.
+    ykeys: ['number_sent'],
+    // Labels for the ykeys -- will be displayed when you hover over the
+    // chart.
+    labels: ['Number of Recipients'],
+    smooth: false
+  });
+};
+
+function responses(){
+  new Morris.Line({
+    // ID of the element in which to draw the chart.
+    element: 'response_chart',
+    data: $('#response_chart').data('response'),
     // The name of the data record attribute that contains x-values.
     xkey: 'date',
     // A list of names of data record attributes that contain y-values.
