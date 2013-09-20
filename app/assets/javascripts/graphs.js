fc0f7d25c34de01f4f9efa28f1c22ef0c26f6830
Andy Hull <andyhull3@gmail.com>
2013-09-20 13:36:13 -0700

    fixed dashboard graphs

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -1,23 +1,28 @@
-graphs = [
-'upcoming',
-'sent',
-'response'
-]
 
-function createGraphs(graphs){
-  graphs.forEach(function (graph) {
-    new Morris.Line({
-      // ID of the element in which to draw the chart.
-      element: graph+'_chart',
-      data: $('#'+graph+'_chart').data(graph),
-      // The name of the data record attribute that contains x-values.
-      xkey: 'date',
-      // A list of names of data record attributes that contain y-values.
-      ykeys: ['number_sent'],
-      // Labels for the ykeys -- will be displayed when you hover over the
-      // chart.
-      labels: ['Number of Recipients'],
-      smooth: false
-    });
-  })
-};
+$(function(){
+  graphs = [
+  'upcoming',
+  'sent',
+  'response'
+  ]
+
+  createGraphs(graphs)
+
+  function createGraphs(graphs){
+    graphs.forEach(function (graph) {
+      new Morris.Line({
+        // ID of the element in which to draw the chart.
+        element: graph+'_chart',
+        data: $('#'+graph+'_chart').data(graph),
+        // The name of the data record attribute that contains x-values.
+        xkey: 'date',
+        // A list of names of data record attributes that contain y-values.
+        ykeys: ['number_sent'],
+        // Labels for the ykeys -- will be displayed when you hover over the
+        // chart.
+        labels: ['Number of Recipients'],
+        smooth: false
+      });
+    })
+  };
+});
