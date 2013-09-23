3db356f8ead2d2b67ce8a3c6fdca2fce3f45ba33
Andy Hull <andyhull3@gmail.com>
2013-09-23 15:37:52 -0700

    new scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,6 @@
 set :output, 'log/cron.log'
 
 every 1.minute do
-  rake "import:cis_test", :environment => 'development'
-end
-
-every 1.day do
-  rake "import:cis_test", :environment => 'staging'
+  rake "import_group[CIS_DATABASE_URL,dc_test_calfresh_english,dc_test_calfresh_english]", :environment => 'development'
+  rake "import_group[CIS_DATABASE_URL,dc_test_calfresh_span,dc_test_calfresh_spanish]", :environment => 'development'
 end
