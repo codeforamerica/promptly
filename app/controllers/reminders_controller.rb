53f73497dca442934b54390a5de06db729ae7488
rcackerman <rebecca@cfa>
2013-09-24 15:20:29 -0400

    Adding confirm page to scheduling reminders.

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -33,35 +33,36 @@ class RemindersController < ApplicationController
     @reminders = Reminder.where("batch_id=?", params[:batch_id])
   end
 
+  def confirm
+    @params = params
+
+    if params[:message]
+      @message = Message.new(params[:message])
+      @message.save
+      params[:reminder][:message_id] = @message.id.to_s
+    end
+
+    if params[:individual_recipients] != ""
+      recipients = parse_phone_numbers(params[:individual_recipients])
+      if params[:create_group] == true
+        create_group_from_individual_recipients(recipients)
+      end
+      recipients.each do |recipient|
+        params[:reminder][:recipient_id] << recipient.to_s
+      end
+    end
+
+    if params[:group_ids] != "" 
+      recipients = group_to_recipient_ids(params[:group_ids])
+      puts recipients
+      recipients.each do |recipient|
+        params[:reminder][:recipient_id] << recipient.to_s
+      end
+    end
+
+  end
 
   def create
-
-    case
-      when params[:message]
-        @message = Message.new(params[:message])
-        @message.save
-        params[:reminder][:message_id] = @message.id.to_s
-
-      when params[:individual_recipients] != ""
-        recipients = parse_phone_numbers(params[:individual_recipients])
-
-        # adding individual recipients to the [:recipient_id] param.
-        recipients.each do |recipient|
-          params[:reminder][:recipient_id] << recipient.to_s
-        end
-
-      when params[:group_ids]
-        if params[:group_ids] != ""
-          # get recipients
-          recipients = group_to_recipient_ids(params[:group_ids])
-          puts recipients
-          # params[:reminder][:recipient_id] << recipient
-          recipients.each do |recipient|
-            params[:reminder][:recipient_id] << recipient.to_s
-          end
-        end
-
-    end
     puts params
 
     params[:reminder][:recipient_id].each do |recipient|
