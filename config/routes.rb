a4a8012f701dcee1eec5b34f5e36c98d64cfe76f
Andy Hull <andyhull3@gmail.com>
2013-09-24 20:48:58 -0700

    added reminder destroy method. this will delete queued rreminders in delayed job.

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Landshark::Application.routes.draw do
     get '/reminders/:batch_id' => 'reminders#show' 
     put '/reminders/:batch_id/edit' => 'reminders#update'
     post '/reminders/:batch_id/edit' => 'reminders#update'
+    delete '/reminders/:batch_id' => 'reminders#destroy'
     resources :reminders
   end
 
