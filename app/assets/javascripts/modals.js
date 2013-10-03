a7d024fbceeefe8cd77ada30f34e68e27547a00b
Andy Hull <andyhull3@gmail.com>
2013-10-03 16:05:10 -0700

    working on saving messages through a modal

--- app/assets/javascripts/modals.js
+++ app/assets/javascripts/modals.js
@@ -4,11 +4,22 @@
 		$('.modal').modal( 'show' );
 		// Lets the cancel button know about the extra fields.
 		$('.modal .cancel-modal').on('click', cancelModal );
-		$('.modal .save-modal').on('click', saveModal);
+    $('.modal').on('hidden', function(){ saveModal(); })
+		// $('.save-modal').on('click', saveModal());
 	});
 
 	function saveModal(event) {
-		var message = $('.modal textarea').val();
+    console.log("i'm saved!");
+		var message = $('#new-message-text-area').val();
+    $.ajax({
+      data: { message_text: message },
+      type: 'post',
+      url: "/admin/messages/"
+    })
+    .done(function(data) {
+      console.log(data);
+    });
+
 		$('.modal').modal( 'hide' );
 	}
 
