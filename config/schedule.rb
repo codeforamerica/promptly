4da0620fff6d7b4197523f553dff0b82418baf18
Jake Solomon <jacob@codeforamerica.org>
2013-10-25 11:19:18 -0700

    added update convo to cron scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,14 @@
 set :output, 'log/cron.log'
 
-every 1.hour do
+every 1.day, :at => '10:00 am' do
   rake "import_group[dc_waived_calfresh_english,dc_waived_calfresh_english]", :environment => 'production'
   rake "import_group[dc_waived_calfresh_spanish,dc_waived_calfresh_spanish]", :environment => 'production'
   rake "import_group[dc_waived_calfresh_chinese,dc_waived_calfresh_chinese]", :environment => 'production'
+  rake "import_group[dc_waived_calfresh_vietnamese,dc_waived_calfresh_vietnamese]", :environment => 'production'
+  rake "import_group[dc_waived_calfresh_russian,dc_waived_calfresh_russian]", :environment => 'production'
+  rake "import_group[dc_waived_calfresh_tagalog,dc_waived_calfresh_tagalog]", :environment => 'production'
+end
+
+every 2.minutes do
+  rake "update_conversations", :environment => 'production'
 end
