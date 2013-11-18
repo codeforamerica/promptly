019cdd8c21fcbfdb9285047fdf712e3b25a8520d
Jake Solomon <jacob@codeforamerica.org>
2013-11-18 10:35:56 -0800

    added import of dc_waived_medical to schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -9,6 +9,7 @@ every 1.day, :at => '10:00 am' do
   rake "import_group[dc_waived_calfresh_tagalog,dc_waived_calfresh_tagalog]", :environment => 'production'
   rake "import_group[dc_waived_calfresh_other,dc_waived_calfresh_other]", :environment => 'production'
   rake "import_group[waived_cases,all_waived_cases]", :environment => 'production'
+  rake "import_group[dc_waived_medical,dc_waived_medical]", :environment => 'production'
 end
 
 every 2.minutes do
