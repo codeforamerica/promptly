97240b353e9f894620666f23eb52da3f5497d85c
Andy Hull <andyhull3@gmail.com>
2014-01-23 16:46:38 -0500

    fixing leftover merge errors.

--- public/assets/bootstrap-datepicker/locales/bootstrap-datepicker.es.js
+++ public/assets/bootstrap-datepicker/locales/bootstrap-datepicker.es.js
@@ -2,8 +2,6 @@
  * Spanish translation for bootstrap-datepicker
  * Bruno Bonamin <bruno.bonamin@gmail.com>
  */
-<<<<<<< HEAD
-
 ;(function($){
 	$.fn.datepicker.dates['es'] = {
 		days: ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"],
@@ -14,6 +12,4 @@
 		today: "Hoy"
 	};
 }(jQuery));
-=======
-(function(a){a.fn.datepicker.dates.es={days:["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado","Domingo"],daysShort:["Dom","Lun","Mar","Mié","Jue","Vie","Sáb","Dom"],daysMin:["Do","Lu","Ma","Mi","Ju","Vi","Sa","Do"],months:["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],monthsShort:["Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep","Oct","Nov","Dic"],today:"Hoy"}})(jQuery);
->>>>>>> hsa
+
