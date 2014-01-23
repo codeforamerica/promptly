d1ac73cd7e2d6e49b94337143668bdf7485c2b14
Andy Hull <andyhull3@gmail.com>
2014-01-23 16:37:19 -0500

    fixing some merge errors

--- public/assets/bootstrap-datepicker/locales/bootstrap-datepicker.es.js
+++ public/assets/bootstrap-datepicker/locales/bootstrap-datepicker.es.js
@@ -2,7 +2,6 @@
  * Spanish translation for bootstrap-datepicker
  * Bruno Bonamin <bruno.bonamin@gmail.com>
  */
-<<<<<<< HEAD
 
 ;(function($){
 	$.fn.datepicker.dates['es'] = {
@@ -14,6 +13,3 @@
 		today: "Hoy"
 	};
 }(jQuery));
-=======
-(function(a){a.fn.datepicker.dates.es={days:["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado","Domingo"],daysShort:["Dom","Lun","Mar","Mié","Jue","Vie","Sáb","Dom"],daysMin:["Do","Lu","Ma","Mi","Ju","Vi","Sa","Do"],months:["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],monthsShort:["Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep","Oct","Nov","Dic"],today:"Hoy"}})(jQuery);
->>>>>>> hsa
