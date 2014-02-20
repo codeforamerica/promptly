d3db0f88c65674f82ad16c2a109bdcdbf5956299
Richa Agarwal <richa@Richas-MacBook-Air.local>
2014-02-20 04:44:43 -0800

    Move roles out of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,17 +10,10 @@ class User < ActiveRecord::Base
 
 	include RoleModel
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :roles, :roles_mask, :name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :roles, :roles_mask, :name, :id
   # attr_accessible :title, :body
 
   validates_presence_of :email
 
-  # optionally set the integer attribute to store the roles in,
-  # :roles_mask is the default
-  roles_attribute :roles_mask
-
-  # declare the valid roles -- do not change the order if you add more
-  # roles later, always append them at the end!
-  roles :admin, :user, :guest
 
 end
