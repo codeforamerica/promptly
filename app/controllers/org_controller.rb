bc558774936303737631945dadb88ed0bf5357ac
Richa Agarwal <richa@Richas-MacBook-Air.local>
2014-02-28 12:07:57 -0800

    Add a filter to get organization user

--- app/controllers/org_controller.rb
+++ app/controllers/org_controller.rb
@@ -1,10 +1,15 @@
 class OrgController < AdminController
 	layout "organization"
 
-  before_filter :get_organization
+  before_filter :get_organization, :get_organization_user
 
   def get_organization
     @organization = Organization.find(params[:organization_id])
+  end
+
+  def get_organization_user 
+  	@user ||= @current_user 
+  	@organization_user = OrganizationsUser.where(user_id: @user.id).where(organization_id: params[:organization_id]).first 
   end
 
   def current_ability
