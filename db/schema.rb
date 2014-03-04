896ff4e130204d96c7a075871c70025730cd1d22
Andy Hull <andyhull3@gmail.com>
2014-03-04 08:48:46 -0500

    fixed modals for adding messages and groups on new reminders page

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140131002017) do
+ActiveRecord::Schema.define(:version => 20140224013918) do
 
   create_table "conversations", :force => true do |t|
     t.datetime "date"
     t.text     "message"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
     t.string   "to_number"
     t.string   "from_number"
     t.string   "message_id"
     t.string   "status"
     t.string   "batch_id"
     t.string   "call_id"
+    t.integer  "organization_id"
   end
 
   create_table "conversations_recipients", :id => false, :force => true do |t|
@@ -55,6 +56,7 @@ ActiveRecord::Schema.define(:version => 20140131002017) do
     t.boolean  "active"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "organization_id"
   end
 
   create_table "groups_recipients", :id => false, :force => true do |t|
@@ -73,10 +75,11 @@ ActiveRecord::Schema.define(:version => 20140131002017) do
 
   create_table "messages", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
     t.string   "message_text"
     t.text     "description"
+    t.integer  "organization_id"
   end
 
   create_table "messages_reminders", :id => false, :force => true do |t|
@@ -103,6 +106,7 @@ ActiveRecord::Schema.define(:version => 20140131002017) do
   create_table "organizations_users", :id => false, :force => true do |t|
     t.integer "organization_id"
     t.integer "user_id"
+    t.integer "roles_mask"
   end
 
   create_table "programs", :force => true do |t|
@@ -143,8 +147,8 @@ ActiveRecord::Schema.define(:version => 20140131002017) do
   create_table "reminders", :force => true do |t|
     t.string   "name"
     t.datetime "send_date"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
     t.integer  "recipient_id"
     t.integer  "message_id"
     t.string   "batch_id"
@@ -153,6 +157,7 @@ ActiveRecord::Schema.define(:version => 20140131002017) do
     t.string   "state"
     t.string   "session_id"
     t.string   "group_ids"
+    t.integer  "organization_id"
   end
 
   create_table "reminders_reports", :id => false, :force => true do |t|
