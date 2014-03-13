a54a0a6dff7e0e59c5061434b48fdd2a6f541417
Andy Hull <andyhull3@gmail.com>
2014-03-13 15:10:03 -0400

    limited scope on dashboard graphs. superdashboard graphs have a totla for the site

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,9 +2,9 @@ class Admin::DashboardController < OrgController
 	
   def index
     @recipient = Recipient.all
-    @recents = Conversation.find(:all, :order => "date desc", :limit => 10)
-    @conversations = Conversation.find(:all, :order => "date desc", :limit => 10)
-    @upcoming = Reminder.grouped_reminders(10).collect
+    @recents = Conversation.find(:all, :order => "date desc")
+    @conversations = Conversation.find(:all, :order => "date desc",)
+    @upcoming = Reminder.organization(@organization.id).grouped_reminders.collect
 
     authorize! :index, @recipient
   end
