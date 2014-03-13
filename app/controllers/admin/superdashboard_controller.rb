11bfe1c709ee840c7a2956a375531755ffb8b6f9
Andy Hull <andyhull3@gmail.com>
2014-03-13 16:04:27 -0400

    added basic monthly text stats to superdashboard.

--- app/controllers/admin/superdashboard_controller.rb
+++ app/controllers/admin/superdashboard_controller.rb
@@ -6,5 +6,7 @@ class Admin::SuperdashboardController < AdminController
     @recents = Conversation.find(:all, :order => "date desc")
     @conversations = Conversation.find(:all, :order => "date desc")
     @upcoming = Reminder.grouped_reminders.collect
+    @last_month = Conversation.where("status =? and date >= ?", "sent", DateTime.now - 1.month )
+    @this_year = Conversation.where("status =? and date >= ?", "sent", Time.now.year )
   end
 end
