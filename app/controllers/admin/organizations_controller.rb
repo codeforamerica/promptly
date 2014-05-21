1384833aec3a61f930e8733f31538aaddee5ffc4
Andy Hull <andyhull3@gmail.com>
2014-05-21 11:06:02 -0400

    fixed error when updating org phone number.

--- app/controllers/admin/organizations_controller.rb
+++ app/controllers/admin/organizations_controller.rb
@@ -30,8 +30,12 @@ class Admin::OrganizationsController < AdminController
   def create
     authorize
     @organization = Organization.new(params[:organization])
-    @phone = "+1" + params[:phone_number]
-    @organization.update_attributes(:phone_number => @phone)
+    if params[:organization][:phone_number].length <12
+      @phone = "+1" + params[:organization][:phone_number]
+      @organization.update_attributes(:phone_number => @phone)
+    else
+      @organization.update_attributes(:phone_number => params[:organization][:phone_number])
+    end
     params[:organizations_user][:user_ids].each do |user_id|
       if user_id[1] == "1"
         @organization_users = OrganizationsUser.where(:organization_id => params[:id], :user_id => user_id[0]).first_or_create
@@ -51,8 +55,12 @@ class Admin::OrganizationsController < AdminController
   def update
     authorize
     @organization = Organization.update(params[:id], params[:organization])
-    @phone = "+1" + params[:organization][:phone_number]
-    @organization.update_attributes(:phone_number => @phone)
+    if params[:organization][:phone_number].length <12
+      @phone = "+1" + params[:organization][:phone_number]
+      @organization.update_attributes(:phone_number => @phone)
+    else
+      @organization.update_attributes(:phone_number => params[:organization][:phone_number])
+    end
     params[:organizations_user][:user_ids].each do |user_id|
       @organization_users
       if user_id[1] == "1"
