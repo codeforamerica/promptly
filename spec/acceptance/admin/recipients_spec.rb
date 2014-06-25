4bddf08b2c0d906d11f74a9bdaa01f9241384ee7
Andy Hull <andyhull3@gmail.com>
2014-06-25 19:52:24 -0400

    fixed recipients_spec.

--- spec/acceptance/admin/recipients_spec.rb
+++ spec/acceptance/admin/recipients_spec.rb
@@ -1,16 +1,20 @@
 require_relative "../acceptance_helper.rb"
 
 feature "Recipients" do
+  before do
+    @user = FactoryGirl.create :user_with_organization
+    @user.organizations_user.first.update_attributes(roles_mask: 1)
+  end
 
   scenario "viewing the index when not logged in" do
-    visit "/admin/recipients"
-    page.current_path.should == "/"
+    visit "/admin/organizations/#{@user.organizations.first.id}/recipients"
+    expect(page.current_path).to eq "/"
   end
 
   scenario "viewing the index page" do
-    sign_in FactoryGirl.create :user
-    visit "/admin/recipients"
-    page.should have_content
+    sign_in @user
+    visit "/admin/organizations/#{@user.organizations.first.id}/recipients"
+    expect(page).to have_content
   end
 
 end
