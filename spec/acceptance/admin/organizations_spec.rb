5ffc3fe93492caabf5f173ad3f2a75c703ab6603
Andy Hull <andyhull3@gmail.com>
2014-06-25 20:12:15 -0400

    fixed messages_spec, organizations_spec, and users_spec.

--- spec/acceptance/admin/organizations_spec.rb
+++ spec/acceptance/admin/organizations_spec.rb
@@ -2,20 +2,21 @@ require_relative '../acceptance_helper'
 
 feature "Organizations" do
   before do
-    @user = FactoryGirl.create :user
+    @user = FactoryGirl.create :user_with_organization
+    @user.organizations_user.first.update_attributes(roles_mask: 1)
   end
 
   it "should load the page when logged in" do
     sign_in @user
-    visit "/admin/organizations"
-    page.current_path.should == "/admin/organizations"
-    page.should have_content "Organizations"
+    visit "/admin/organizations/#{@user.organizations.first.id}/organizations"
+    expect(page.current_path).to eq "/admin/organizations/#{@user.organizations.first.id}/organizations"
+    expect(page).to have_content "Organizations"
   end
 
   scenario "should load the creation page" do
     sign_in @user
-    visit "/admin/organizations/new"
-    page.current_path.should == "/admin/organizations/new"
-    page.should have_content "New Organization"
+    visit "/admin/organizations/#{@user.organizations.first.id}/organizations/new"
+    expect(page.current_path).to eq "/admin/organizations/#{@user.organizations.first.id}/organizations/new"
+    expect(page).to have_content "New Organization"
   end
 end
--- spec/acceptance/admin/users_spec.rb
+++ spec/acceptance/admin/users_spec.rb
@@ -1,33 +1,37 @@
 require_relative "../acceptance_helper.rb"
 
 feature "Users" do
+  before do
+    @user = FactoryGirl.create :user_with_organization
+    @user.organizations_user.first.update_attributes(roles_mask: 1)
+  end
 
   scenario "viewing the index when not logged in" do
-    visit "/admin/users"
-    page.current_path.should == "/"
+    visit "/admin/organizations/#{@user.organizations.first.id}/users"
+    expect(page.current_path).to eq "/"
   end
 
   scenario "viewing the index page" do
-    sign_in FactoryGirl.create :user
-    visit "/admin/users"
-    page.should have_content
+    sign_in @user
+    visit "/admin/organizations/#{@user.organizations.first.id}/users"
+    expect(page).to have_content
   end
 
    scenario "creating a new user when not logged in" do
-    visit "/admin/organizations/1/users/new"
-    page.current_path.should == "/"
+    visit "/admin/organizations/#{@user.organizations.first.id}/users/new"
+    expect(page.current_path).to eq "/"
   end
 
   scenario "creating a new user" do
-    sign_in FactoryGirl.create :user
-    visit "/admin/users/new"
-    page.should have_content
+    sign_in @user
+    visit "/admin/organizations/#{@user.organizations.first.id}/users/new"
+    expect(page).to have_content
   end
 
   scenario "viewing the show page" do
-    sign_in FactoryGirl.create :user
-    visit "/admin/organizations/1/users/1"
-    page.should have_content
+    sign_in @user
+    visit "/admin/organizations/#{@user.organizations.first.id}/users/1"
+    expect(page).to have_content
   end
     
 end
