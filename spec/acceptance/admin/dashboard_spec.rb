3d49620513cf802d96fe7fcb80610d24b2ae650d
Andy Hull <andyhull3@gmail.com>
2014-06-25 20:01:54 -0400

    fixed conversations_spec and dashboard_spec.

--- spec/acceptance/admin/dashboard_spec.rb
+++ spec/acceptance/admin/dashboard_spec.rb
@@ -1,20 +1,25 @@
 require_relative '../acceptance_helper'
 
 feature "Dashboard" do
+  before do
+    @user = FactoryGirl.create :user_with_organization
+    @user.organizations_user.first.update_attributes(roles_mask: 1)
+  end
+
   scenario "showing the page when not logged in" do
-    visit "/admin"
-    page.current_path.should == "/"
-    page.should have_content "Promptly"
+    visit "/admin/organizations/#{@user.organizations.first.id}/"
+    expect(page.current_path).to eq "/"
+    expect(page).to have_content "Promptly"
   end
 
   scenario "showing the page when logged in" do
-    sign_in FactoryGirl.create :user
-    visit "/admin"
-    page.current_path.should == "/admin"
-    page.should have_content "Schedule a reminder"
-    page.should have_content "Upcoming reminders"
-    page.should have_content "Sent reminders"
-    page.should have_content "Responses"
-    page.should have_content "Undelivered reminders"
+    sign_in @user
+    visit "/admin/organizations/#{@user.organizations.first.id}/dashboard"
+    expect(page.current_path).to eq "/admin/organizations/#{@user.organizations.first.id}/dashboard"
+    expect(page).to have_content "Schedule a reminder"
+    expect(page).to have_content "Upcoming reminders"
+    expect(page).to have_content "Sent reminders"
+    expect(page).to have_content "Responses"
+    expect(page).to have_content "Undelivered reminders"
   end
 end
