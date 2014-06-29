6727df0703aece331e80017659359da8b1fa3a42
Andy Hull <andyhull3@gmail.com>
2014-06-29 10:07:46 -0400

    adding sendgrid support to promptly.io

--- config/environment.rb
+++ config/environment.rb
@@ -10,3 +10,13 @@ Time::DATE_FORMATS[:date_format] = lambda { |time| time.strftime("%B %d, %Y %l:%
 Time::DATE_FORMATS[:time_only] = lambda { |time| time.strftime("%l:%M%p %Z") }
 Time::DATE_FORMATS[:date_only] = lambda { |time| time.strftime("%B %d, %Y") }
 Time::DATE_FORMATS[:input_format] = "%yy %mm %dd %H:%M"
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
