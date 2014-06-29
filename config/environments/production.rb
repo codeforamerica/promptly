85163ed706747c8fe0eb1d7704ed70d94cfbe57e
Andy Hull <andyhull3@gmail.com>
2014-06-29 14:34:22 -0400

    updating email host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Promptly::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'promptly.io' }
+  config.action_mailer.default_url_options = { :host => 'www.promptly.io' }
 
 
   # Enable threaded mode
