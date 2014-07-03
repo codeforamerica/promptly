115ec0ab164acf8a780711b28cadc0501052ebb6
Andy Hull <andyhull3@gmail.com>
2014-07-03 11:25:04 -0400

    added phony validation to organization phone number. Fixes #378.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,7 +2,8 @@ class Organization < ActiveRecord::Base
   attr_accessible :name, :user_ids, :recipient_ids, :conversation_ids, :group_ids, :message_ids, :reminder_ids, :organizations_user, :phone_number
   has_many :organizations_user
   has_many :users, through: :organizations_user
-  validates_presence_of :name, :phone_number
+  validates_presence_of :name
+  validates_plausible_phone :phone_number, :presence => true
   has_many :conversations
   has_many :groups
   has_many :messages
