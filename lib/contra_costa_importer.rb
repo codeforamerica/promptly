b917338ea59f1de564539bbe44a5f7985eaec386
CJ Bryan <christianbryan@gmail.com>
2014-07-27 16:47:18 -0700

    Create or find the reminder when importing.

--- lib/contra_costa_importer.rb
+++ lib/contra_costa_importer.rb
@@ -5,6 +5,9 @@ require 'pry'
 class ContraCostaImporter
   attr_accessor :content
 
+  # What organization should these appointments be imported into?
+  ORGANIZATION_ID = 1
+
   # Keys are a combo of a message code from CalWIN and a language code.
   # Values are message ID's in the Promptly DB.
   # `nil` means we don't know the message id yet.
@@ -14,6 +17,8 @@ class ContraCostaImporter
     '100SP' => 20,
     '300SP' => nil
   }
+
+  DATE_FORMAT = '%F %H:%M:%S'
 
   def initialize(path)
     @content = File.read(path)
@@ -37,10 +42,22 @@ class ContraCostaImporter
     group = Group.where(name: build_group_name(appointment[:appt_datetime])).first_or_create
     group.description = 'Built automatically by The Importer.'
 
+    message = get_message(appointment)
+    
+    # TODO(christianbryan@gmail.com): There could be some serious time zone issues here.
+    date = Time.strptime(DATE_FORMAT, appointment[:appt_datetime])
+    reminder = Reminder.where(name: 'Importer Job', organization_id: ORGANIZATION_ID).first_or_create
+    reminder.send_date = date
+    reminder.send_time = date
+
     puts group.inspect
   end
 
   def build_group_name(date)
     "Imported: #{date}"
+  end
+
+  def get_message(appointment)
+    Message.where(name: appointment[:mssg_cd] + appointment[:language]).first_or_create
   end
 end
