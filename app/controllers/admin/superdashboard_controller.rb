3b09f684c30f1c9dd8fcefbbd116a910575cb1b5
andyhull <andyhull3@gmail.com>
2014-12-09 15:46:06 -0500

    added workflow for adding super admin users.

--- app/controllers/admin/superdashboard_controller.rb
+++ app/controllers/admin/superdashboard_controller.rb
@@ -12,4 +12,45 @@ class Admin::SuperdashboardController < AdminController
     @last_month == 0 ? @response_rate = 0 : @response_rate = ((Conversation.month_calls.count.to_f/2)/@last_month.to_f)*100
     @undelivered_rate = Conversation.undelivered_month.count >0? (Conversation.undelivered_month.count.to_f/@last_month.to_f)*100 : 0
   end
+
+  def show_users
+    @users = User.all
+  end
+
+  def edit_user
+    @user = User.find(params[:id])
+  end
+
+  def update_user
+    @user = User.find(params[:id])
+    if params[:user][:password].blank?
+      [:password,:password_confirmation,:current_password].collect{|p| params[:user].delete(p) }
+    end
+    account_update_params = params[:user]
+     # required for settings form to submit when password is left blank
+    if account_update_params[:password].blank?
+      account_update_params.delete("password")
+      account_update_params.delete("password_confirmation")
+    end
+    if @current_user.id == @user.id
+      @bypass_flag = true
+    else
+      @bypass_flag = false
+    end
+
+    respond_to do |format|
+      if @user.errors[:base].empty? and @user.update_attributes(params[:user])
+        if @bypass_flag == true
+          sign_in(@user, :bypass => true)
+        end
+          format.json { render :json => @user.to_json, :status => 200 }
+          format.xml  { head :ok }
+          format.html { redirect_to admin_superdashboard_show_users_path(1), notice: 'User account has been updated' }
+      else
+        format.json { render :text => "Could not update user", :status => :unprocessable_entity } #placeholder
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+        format.html { render :text => @user.errors.full_messages.join('<br/>'), :status => :unprocessable_entity }
+      end
+    end
+  end
 end
